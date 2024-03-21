79969a3819d29ffe9b416f5e670d71dd23d65b3a
ironpenguin222 <ironpenguin333@gmail.com>
2024-03-21 00:14:05 -0400

    Task 4a done

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,16 +1,39 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterControl : MonoBehaviour
 {
-    public TMPro.TextMeshProUGUI currentSelection;
+    public TMP_Dropdown dropdown;
+    public TextMeshProUGUI currentSelection;
     public static CharacterControl instance;
     public static Villager SelectedVillager { get; private set; }
+
+    public List<Villager> availableVillagers; // List of available villagers to assign
+
+    private int previousDropdownValue = -1;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        int currentDropdownValue = dropdown.value;
+        if (currentDropdownValue != previousDropdownValue && currentDropdownValue >= 0 && currentDropdownValue < availableVillagers.Count)
+        {
+            previousDropdownValue = currentDropdownValue;
+            SetSelectedVillager(availableVillagers[currentDropdownValue]);
+        }
+    }
+
     public static void SetSelectedVillager(Villager villager)
     {
-        if(SelectedVillager != null)
+        if (SelectedVillager != null)
         {
             SelectedVillager.Selected(false);
         }
@@ -18,10 +41,6 @@ public class CharacterControl : MonoBehaviour
         SelectedVillager.Selected(true);
         instance.currentSelection.text = villager.ToString();
     }
-
-    public void Start()
-    {
-        instance = this;
-    }
-
 }
+
+
